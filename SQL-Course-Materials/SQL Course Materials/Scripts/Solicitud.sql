789ded4ac0b28db4aa4232d82d6e78740aef7937
Gustavo V. Diaz <gus838@gmail.com>
2020-07-13 18:59:53 -0300

    Modifico el script para que solo levante la columna 'state' de la tabla 'customers'

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,7 +1,2 @@
-SELECT customer_id,
-		first_name,
-		last_name,
-        city,
-        points * 10 + 100 AS 'Aplicado Descuento',
-        points
-FROM sql_store.customers
+SELECT state
+FROM customers 
