cd0c68c001ea6388ac677b532d59fbf651b765cf
Gustavo V. Diaz <gus838@gmail.com>
2020-07-13 19:29:56 -0300

    Ejercicio propuesto en búsqueda de base de datos

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,4 +1,4 @@
--- Uso de la palabra especial WHERE, la cual selecciona las entradas por una condición de una columna
-SELECT *
-FROM customers
-WHERE state = 'VA'
+-- Ejercicio propuesto en minuto 42:33 del curso https://www.youtube.com/watch?v=7S_tz1z_5bA&t=624s
+SELECT * 
+FROM sql_store.orders
+WHERE order_date > '2018-12-01'
