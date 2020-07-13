6af61a6557ebeef782f7f532bf768cb1aea04d07
Gustavo V. Diaz <gus838@gmail.com>
2020-07-13 18:54:20 -0300

    Modificación de script para ponerle un rótulo a la columna que se vió modificada por el operador matemático

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -2,6 +2,6 @@ SELECT customer_id,
 		first_name,
 		last_name,
         city,
-        points * 10 + 100,
+        points * 10 + 100 AS 'Aplicado Descuento',
         points
 FROM sql_store.customers
