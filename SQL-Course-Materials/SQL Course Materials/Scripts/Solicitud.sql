e9c2235e7162047a1c129b72434f8e1ca71b2655
Gustavo V. Diaz <gus838@gmail.com>
2020-07-16 20:23:08 -0300

    Ejercicio sobre uso de sentencia BETWEEN, guarda con el formato de las fechas

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,4 +1,5 @@
--- Uso de sentencia BETWEEN
+-- Ejercicio: traer de los "customers" que nacieron entre 1/1/1990 y 1/1/2000
 SELECT * 
 FROM sql_store.customers
-WHERE points BETWEEN 1000 AND 3000
+-- Guarda con el formato que debe tener la fecha de nacimiento
+WHERE birth_date BETWEEN '1990-01-01' AND '2000-01-01'
