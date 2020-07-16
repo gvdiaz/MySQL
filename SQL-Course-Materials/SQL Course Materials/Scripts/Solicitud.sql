a8f2e2b0b5c08e3f465708fb10cdf7dce8016f1c
Gustavo V. Diaz <gus838@gmail.com>
2020-07-16 20:54:26 -0300

    Anoté el minuto donde dejé el curso

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,4 +1,5 @@
 -- Uso de REGEXP (Expresión regular), el mismo en este ejercicio indica que en cualquier lado donde esté "field" va a devolver el item
+-- Me quedé en el minuto 1:04:26
 SELECT * 
 FROM sql_store.customers
 -- '^' significa que la cadena de caracteres empieza con field
