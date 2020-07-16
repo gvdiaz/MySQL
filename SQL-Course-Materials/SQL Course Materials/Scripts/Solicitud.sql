32c2f7e3f4acc123405b9676b33e6ba246fac043
Gustavo V. Diaz <gus838@gmail.com>
2020-07-16 20:34:19 -0300

    Uso del signo _

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,5 +1,5 @@
--- Uso de la sentencia LIKE y el signo %, en este caso va a devolver los items que tengan en el apellido aunque sea una b
+-- Uso de la sentencia LIKE y el signo _
 SELECT * 
 FROM sql_store.customers
--- El modificador % indica que luego de la 'b' puede haber cualquier cantidad de letras detrás de ella
-WHERE last_name LIKE '%b%' 
+-- El modificador _ indica que ese espacio puede ser reemplazado por la cantidad de espacios que se ponga
+WHERE last_name LIKE '_____y' 
