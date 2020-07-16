63d391ebeccd00660d90971291d97ee6d96671d9
Gustavo V. Diaz <gus838@gmail.com>
2020-07-16 20:29:15 -0300

    Uso de la sentencia LIKE junto con el comodín %

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,5 +1,5 @@
--- Ejercicio: traer de los "customers" que nacieron entre 1/1/1990 y 1/1/2000
+-- Uso de la sentencia LIKE
 SELECT * 
 FROM sql_store.customers
--- Guarda con el formato que debe tener la fecha de nacimiento
-WHERE birth_date BETWEEN '1990-01-01' AND '2000-01-01'
+-- El modificador % indica que luego de la 'b' puede haber cualquier cantidad de letras detrás de ella
+WHERE last_name LIKE 'b%' 
