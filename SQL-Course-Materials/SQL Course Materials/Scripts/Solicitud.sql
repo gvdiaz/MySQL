9e0f51ce2dad6b16bede1aaffe3a53afc3d975c4
Gustavo V. Diaz <gus838@gmail.com>
2020-07-16 20:32:14 -0300

    Uso especial del signo % y con la sentencia LIKE

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,5 +1,5 @@
--- Uso de la sentencia LIKE
+-- Uso de la sentencia LIKE y el signo %, en este caso va a devolver los items que tengan en el apellido aunque sea una b
 SELECT * 
 FROM sql_store.customers
 -- El modificador % indica que luego de la 'b' puede haber cualquier cantidad de letras detrás de ella
-WHERE last_name LIKE 'b%' 
+WHERE last_name LIKE '%b%' 
