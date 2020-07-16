d8f7e59902a97dcee9e66864f1b30139920d85f4
Gustavo V. Diaz <gus838@gmail.com>
2020-07-16 20:52:32 -0300

    Uso de signo ^ dentro de REGEXP

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,6 +1,6 @@
 -- Uso de REGEXP (Expresión regular), el mismo en este ejercicio indica que en cualquier lado donde esté "field" va a devolver el item
 SELECT * 
 FROM sql_store.customers
-
-WHERE last_name REGEXP 'field'
+-- '^' significa que la cadena de caracteres empieza con field
+WHERE last_name REGEXP '^field'
 
