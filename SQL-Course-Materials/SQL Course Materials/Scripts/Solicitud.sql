81421da3dac0fcceeefe6a44f7061fd1ede5fe46
Gustavo V. Diaz <gus838@gmail.com>
2020-07-17 19:25:33 -0300

    Uso de la sentencia JOIN, la cual une los contenidos de dos columnas, también se usó el alias

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,5 +1,7 @@
--- Ejercicio: Traer a los tres clientes más leales (que tengan más puntos)
-SELECT *
-FROM sql_store.customers
-ORDER BY points DESC
-LIMIT 3 -- El primer número es un offset y el segundo la cantidad de items a traer
+-- Uso de sentencia JOIN
+SELECT order_id, o.customer_id, first_name, last_name
+FROM sql_store.orders o
+JOIN sql_store.customers c ON o.customer_id = c.customer_id
+
+# Esta sentencia se encarga de unir los valores contenidos la columna "customer_id" de la tabla
+# "orders" con los de la tabla "customer_id" de la tabla "customers"
