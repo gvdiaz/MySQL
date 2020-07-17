668fac32c51f8e00c42e73a59bd80a77b51f7be9
Gustavo V. Diaz <gus838@gmail.com>
2020-07-17 18:17:47 -0300

    Ejercicio 3 de REGEXP

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,4 +1,4 @@
--- Ejercicio 2: Traer los clientes que su apellido termine con EY o ON
+-- Ejercicio 3: Traer los clientes que su apellido empiece con MY o contenga SE
 SELECT * 
 FROM sql_store.customers
-WHERE last_name REGEXP 'ON$|EY$'
+WHERE last_name REGEXP '^MY|SE'
