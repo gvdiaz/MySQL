d68466e8df8ca4a3743aee9d0209b45d133bf11f
Gustavo V. Diaz <gus838@gmail.com>
2020-07-20 18:53:51 -0300

    Ejercicio de JOIN de una tabla consigo misma

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,6 +1,10 @@
--- Ejercicio JOIN: Requerir item de la tabla order_items order_id, product_id agregar nombre del
-# producto obtenido de products y terminar con  quantity y unit_price
-# Dejé en minuto 1:36:00
-SELECT o.order_id, o.product_id, p.name, o.quantity, o.unit_price
-FROM sql_store.order_items o
-JOIN sql_store.products p ON o.product_id = p.product_id
+-- Uso JOIN con una tabla entre si
+USE sql_hr;
+
+SELECT 	e.employee_id,
+		e.first_name,
+		e.last_name,
+        m.last_name AS Manager
+
+FROM employees e
+JOIN employees m ON e.reports_to = m.employee_id
