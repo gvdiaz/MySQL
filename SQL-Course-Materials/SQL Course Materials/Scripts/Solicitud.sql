078762a174f2d94eb75ca3217b88447508fdeed4
Gustavo V. Diaz <gus838@gmail.com>
2020-07-23 19:05:31 -0300

    Ejercicio de JOIN con tres tablas, solo un poco más complicado

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,11 +1,14 @@
--- Uso JOIN con una tabla entre si
--- Comentario de avance hasta minuto 1:40:25
-USE sql_hr;
+-- Uso JOIN con dos tablas
+-- Ejercicio propuesto de 1:41:33
 
-SELECT 	e.employee_id,
-		e.first_name,
-		e.last_name,
-        m.last_name AS Manager
+USE sql_store;
 
-FROM employees e
-JOIN employees m ON e.reports_to = m.employee_id
+SELECT 	o.order_id AS Orden_id,
+		o.order_date AS Fecha_orden,
+        c.first_name AS Nombre,
+        c.last_name AS Apellido,
+        os.name AS Estado
+
+FROM orders o
+JOIN customers c ON o.customer_id = c.customer_id
+JOIN order_statuses os ON o.status = os.order_status_id
