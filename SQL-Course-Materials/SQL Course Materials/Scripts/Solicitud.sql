7dcf0f00484071acdb7f1dcaa2c8ec52a015a401
Gustavo V. Diaz <gus838@gmail.com>
2020-07-23 20:07:06 -0300

    Ejercicio de LEFT JOIN

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,14 +1,14 @@
--- Uso de OUTER JOIN (LEFT o RIGHT)
+-- Ejercicio de OUTER JOIN (LEFT o RIGHT)
 
--- Ejercicio propuesto de 1:56:00 (Eh mas o menos
+-- Ejercicio propuesto de 1:58:00
 
 USE sql_store;
 
 SELECT	
-		c.customer_id,
-		c.first_name,
-        o.order_id
+		p.product_id AS Id_producto, -- Tabla order_items
+        p.name AS Nombre_producto, -- Tabla products
+        oi.quantity AS cantidad -- Tabla order_items
 
-FROM customers c
-RIGHT JOIN orders o ON c.customer_id = o.customer_id
-ORDER BY c.customer_id
+FROM order_items oi
+right JOIN products p ON oi.product_id = p.product_id
+ORDER BY p.product_id
