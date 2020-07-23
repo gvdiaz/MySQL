10cb81513eb69b86a4be43ff3ee522c5d65b6c24
Gustavo V. Diaz <gus838@gmail.com>
2020-07-23 20:49:08 -0300

    Uso de la sentencia USING

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,21 +1,15 @@
--- Ejercicio de OUTER JOIN (LEFT o RIGHT)
+-- Uso de sentencia USING -> Se utiliza para unir (JOIN) dos columnas en distintas tablas pero que tienen la misma denominación
 
--- Ejercicio propuesto de 2:03:00
+-- Ejercicio propuesto de 2:09:00
 
 USE sql_store;
 
 SELECT	
-		o.order_date AS Orden_fecha,	-- Tabla order_items
-        o.order_id AS ID_orden,		-- Tabla order_item
-        c.first_name AS Nombre_cliente,	-- Tabla customers
-        sh.name AS Nombre_embarque, 	-- Tabla shippers
-        os.name AS Estado				-- Tabla order_statuses
-
+		o.order_id,
+        c.first_name
+        
 FROM orders o
 JOIN customers c
-	ON o.customer_id = c.customer_id
-LEFT JOIN shippers sh
-	ON o.shipper_id = sh.shipper_id
-JOIN order_statuses os
-	ON o.status = os.order_status_id
-ORDER BY sh.name
+	-- ON o.customer_id = c.customer_id
+    USING (customer_id) -- Esta línea reemplaza a la anterior (es más corta y más legible.
+    
