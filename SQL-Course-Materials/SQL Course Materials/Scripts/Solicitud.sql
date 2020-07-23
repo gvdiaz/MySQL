1cf12136779cc5e13f531246be0bb07a6498142a
Gustavo V. Diaz <gus838@gmail.com>
2020-07-23 20:38:26 -0300

    Ejercicio de LEFT JOIN, un poquito más complicado

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,14 +1,21 @@
 -- Ejercicio de OUTER JOIN (LEFT o RIGHT)
 
--- Ejercicio propuesto de 1:58:00
+-- Ejercicio propuesto de 2:03:00
 
 USE sql_store;
 
 SELECT	
-		p.product_id AS Id_producto, -- Tabla order_items
-        p.name AS Nombre_producto, -- Tabla products
-        oi.quantity AS cantidad -- Tabla order_items
+		o.order_date AS Orden_fecha,	-- Tabla order_items
+        o.order_id AS ID_orden,		-- Tabla order_item
+        c.first_name AS Nombre_cliente,	-- Tabla customers
+        sh.name AS Nombre_embarque, 	-- Tabla shippers
+        os.name AS Estado				-- Tabla order_statuses
 
-FROM order_items oi
-right JOIN products p ON oi.product_id = p.product_id
-ORDER BY p.product_id
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id
+LEFT JOIN shippers sh
+	ON o.shipper_id = sh.shipper_id
+JOIN order_statuses os
+	ON o.status = os.order_status_id
+ORDER BY sh.name
