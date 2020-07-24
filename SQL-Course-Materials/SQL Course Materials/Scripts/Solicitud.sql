cd8a1b8651ad35a65b3e4917f0c759b1c91db31f
Gustavo V. Diaz <gus838@gmail.com>
2020-07-24 17:31:15 -0300

    Ejercicio de CROSS JOIN expresado de manera explícita

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,13 +1,15 @@
--- Uso de sentencia CROSS JOINI -> Se utiliza para unir (JOIN) dos tablas y relacionar todas las combinaciones posibles
+-- Ejercicio CROSS JOIN: Hacer un CROSS JOIN entre shippers y products de manera explícita
 
--- Ejercicio propuesto de 2:16:00
+-- Ejercicio propuesto de 2:17:00
 
 USE sql_store;
 
 SELECT	
-		c.first_name AS consumidor,
-        p.name AS product
+		sh.name AS Empresa_envío,
+        p.name AS Productos
+		-- c.first_name AS consumidor,
+        -- p.name AS product
         
-FROM customers c
+FROM shippers sh
 CROSS JOIN products p
-ORDER BY c.first_name
+ORDER BY sh.name
