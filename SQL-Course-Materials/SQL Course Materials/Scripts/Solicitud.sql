eec2b96ade781526fe8d9cbb5085bc5f072b1464
Gustavo V. Diaz <gus838@gmail.com>
2020-07-24 18:35:31 -0300

    Ejercicio de UNION

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,23 +1,38 @@
--- Uso de UNION: Une dos búsquedas
+-- Ejercicio de UNION: Crear una tabla donde califique según la puntuación del consumidor. menos de 2000 bronce, entre 2000 y 3000 plata y mas de 3000 oro
 
--- Ejercicio propuesto de 2:21:00
+-- Ejercicio propuesto de 2:26:00
 
 USE sql_store;
 
 SELECT	
-		order_id AS Id_orden,
-        order_date AS Productos,
-        'Activo' AS Estado
+		customer_id AS Id_consumidor,
+        first_name AS Nombre,
+        points AS Puntos,
+        'Bronce' AS Calificación
         
-FROM orders
-WHERE order_date >= '2019-01-01'
+FROM customers
+WHERE (points < 2000)
 
 UNION
 
 SELECT	
-		order_id AS Id_orden,
-        order_date AS Productos,
-        'Archivado' AS Estado
+		customer_id AS Id_consumidor,
+        first_name AS Nombre,
+        points AS Puntos,
+        'Plata' AS Calificación
         
-FROM orders
-WHERE order_date < '2019-01-01'
+FROM customers
+WHERE (points BETWEEN 2000 AND 3000)
+
+UNION
+
+SELECT	
+		customer_id AS Id_consumidor,
+        first_name AS Nombre,
+        points AS Puntos,
+        'Oro' AS Calificación
+        
+FROM customers
+WHERE (points > 3000)
+
+ORDER BY Nombre
