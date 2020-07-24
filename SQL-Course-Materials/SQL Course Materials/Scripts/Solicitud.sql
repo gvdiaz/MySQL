2b49731e5b358e7dd5a3660bc11db77b5b22f580
Gustavo V. Diaz <gus838@gmail.com>
2020-07-24 18:59:10 -0300

    Uso 1 de sentencia INSERT

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,38 +1,21 @@
--- Ejercicio de UNION: Crear una tabla donde califique según la puntuación del consumidor. menos de 2000 bronce, entre 2000 y 3000 plata y mas de 3000 oro
+-- Uso de INSERT: Inserta una fila
 
--- Ejercicio propuesto de 2:26:00
+-- Ejercicio propuesto de 2:35:00
 
 USE sql_store;
 
-SELECT	
-		customer_id AS Id_consumidor,
-        first_name AS Nombre,
-        points AS Puntos,
-        'Bronce' AS Calificación
-        
-FROM customers
-WHERE (points < 2000)
+INSERT INTO customers (
+	first_name,
+    last_name,
+    birth_date,
+    address,
+    city,
+    state)
+VALUES (
+	'John',
+    'Frusciante',
+    DEFAULT,
+    'Andonaegui 200',
+    'La Ferrere',
+    'CN')
 
-UNION
-
-SELECT	
-		customer_id AS Id_consumidor,
-        first_name AS Nombre,
-        points AS Puntos,
-        'Plata' AS Calificación
-        
-FROM customers
-WHERE (points BETWEEN 2000 AND 3000)
-
-UNION
-
-SELECT	
-		customer_id AS Id_consumidor,
-        first_name AS Nombre,
-        points AS Puntos,
-        'Oro' AS Calificación
-        
-FROM customers
-WHERE (points > 3000)
-
-ORDER BY Nombre
