0b912ad8a6a0db4ddbbfd039c2831605a13a2592
Gustavo V. Diaz <gus838@gmail.com>
2020-07-25 15:02:14 -0300

    Ejercicio de creación de tabla y truncado de la misma (invoice_archived)

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,11 +1,9 @@
--- Creación de una base de datos en base a otra, la trunco y ahora le inserto ciertos valores de la tabla, para ser más precisos los datos de antes del 2018
--- Efecto: Le borra todos los datos a la nueva base
+-- Ejercicio copiar base de datos invoices pero solo los que hayan pagado
+-- Paso 1: Copio la tabla
 
 -- Ejercicio propuesto de 2:48:00
 
-USE sql_store;
-
-INSERT INTO order_archived
+USE sql_invoicing;
+CREATE TABLE invoices_payed
 SELECT *
-FROM orders
-WHERE order_date < '2018-01-01'
+FROM invoices
