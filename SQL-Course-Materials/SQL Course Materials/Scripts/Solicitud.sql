11215d5ed99c8449ac34c0393bcbe9d5e7c994fe
Gustavo V. Diaz <gus838@gmail.com>
2020-07-25 16:01:53 -0300

    Ejercicio de UPDATE

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,9 +1,13 @@
--- Ejercicio de UPDATE: Dar a los customers nacidos antes de 1990 50 puntos extra
--- Ejercicio propuesto de 3:00:00
+-- Ejercicio de UPDATE: Agregar comentario en tabla orders cuando el consumidor supere los 3000 punto
+-- Ejercicio propuesto de 3:05:00
 
 USE sql_store;
 
-UPDATE customers
+UPDATE orders
 SET
-	points = points + 50
-WHERE birth_date < '1990-01-01'
+	comments = 'Vo sos crack + 3000 ptos'
+WHERE customer_id IN
+					(SELECT customer_id
+                    FROM customers
+                    WHERE points > 3000)
+
