57c52d6d7241a89219e6656f679a2bb568b458be
Gustavo V. Diaz <gus838@gmail.com>
2020-07-25 14:37:12 -0300

    Uso del INSERT con jerarquía, tuve varios problemas y modifiqué de lo lindo las bases de datos hasta poder agregar dichas inserción con jerarquía

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,15 +1,20 @@
--- Ejercicio INSERT: Ingresar tres filas en la tabla de "products"
+-- Ejercicio INSERT con jerarquía: Lo voy a aplicar para agregar una orden la cual tiene hijos que se deben completar una vez que se inicia una orden
 
 -- Ejercicio propuesto de 2:37:00
 
 USE sql_store;
 
-INSERT INTO products (
-    name,
-    quantity_in_stock,
-    unit_price)
+INSERT INTO orders (
+    customer_id,
+    order_date,
+    status)
     
-VALUES 	('Arroz',5,0.5),
-		('Arroz carnolli',10,0.7),
-        ('Spaguetti',10,0.5)
+VALUES 	(10,'2019-01-02',DEFAULT);
+
+-- SELECT last_insert_id()
+INSERT INTO order_items 
+VALUES
+	(last_insert_id(), 1, 1, 2.95),
+	(LAST_INSERT_ID(), 2, 1, 3.95)
+    
 
