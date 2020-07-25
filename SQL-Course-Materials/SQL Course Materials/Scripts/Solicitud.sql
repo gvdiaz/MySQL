921589576d29f2520b160ecc4dcd94e18e80f406
Gustavo V. Diaz <gus838@gmail.com>
2020-07-25 16:11:15 -0300

    Uso de comando DELETE

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,13 +1,11 @@
 -- Ejercicio de UPDATE: Agregar comentario en tabla orders cuando el consumidor supere los 3000 punto
 -- Ejercicio propuesto de 3:05:00
 
-USE sql_store;
+USE sql_invoicing;
 
-UPDATE orders
-SET
-	comments = 'Vo sos crack + 3000 ptos'
-WHERE customer_id IN
-					(SELECT customer_id
-                    FROM customers
-                    WHERE points > 3000)
+DELETE FROM invoices
+WHERE client_id =
+		(SELECT client_id
+		FROM clients
+		WHERE name = 'Myworks')
 
