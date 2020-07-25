db7d47700cc4cb5c277a221f56351fd794e0e1e3
Gustavo V. Diaz <gus838@gmail.com>
2020-07-25 14:55:26 -0300

    Agregado de datos en order_archived con datos de antes de 2018

--- SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
+++ SQL-Course-Materials/SQL Course Materials/Scripts/Solicitud.sql
@@ -1,9 +1,11 @@
--- Creación de una base de datos en base a otra
--- Efecto: Crea la base de datos completa en base a orders
+-- Creación de una base de datos en base a otra, la trunco y ahora le inserto ciertos valores de la tabla, para ser más precisos los datos de antes del 2018
+-- Efecto: Le borra todos los datos a la nueva base
 
--- Ejercicio propuesto de 2:47:00
+-- Ejercicio propuesto de 2:48:00
 
 USE sql_store;
 
-CREATE TABLE order_archived AS
-SELECT * FROM orders
+INSERT INTO order_archived
+SELECT *
+FROM orders
+WHERE order_date < '2018-01-01'
